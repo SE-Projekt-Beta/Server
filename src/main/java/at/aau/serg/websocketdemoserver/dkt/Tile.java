2bb20aa2972d9041a65ba7e81d7b7aef0f417e89
Benjamin Bordne <benjibordne@outlook.com>
2025-04-03 18:54:51 +0200

    Geht jetzt mit dem Board an client schicken

--- src/main/java/at/aau/serg/websocketdemoserver/dkt/Tile.java
+++ src/main/java/at/aau/serg/websocketdemoserver/dkt/Tile.java
@@ -1,5 +1,22 @@
 package at.aau.serg.websocketdemoserver.dkt;
+import com.fasterxml.jackson.annotation.*;
+import at.aau.serg.websocketdemoserver.dkt.tiles.*;
 
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = Event.class, name = "event"),
+        @JsonSubTypes.Type(value = Free.class, name = "free"),
+        @JsonSubTypes.Type(value = GoToJail.class, name = "goToJail"),
+        @JsonSubTypes.Type(value = Jail.class, name = "jail"),
+        @JsonSubTypes.Type(value = Start.class, name = "start"),
+        @JsonSubTypes.Type(value = Station.class, name = "station"),
+        @JsonSubTypes.Type(value = Street.class, name = "street"),
+        @JsonSubTypes.Type(value = Tax.class, name = "tax"),
+})
 public class Tile {
     private final int position;
     private final String name;
