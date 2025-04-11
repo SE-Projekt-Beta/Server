2e16df5c214de55e82b1665d2e4429a1760e9cb8
Thomas <thomasel@edu.aau.at>
2025-04-11 08:31:57 +0200

    enums: message-type

---
+++ src/main/java/at/aau/serg/websocketdemoserver/dto/MessageType.java
@@ -0,0 +1,19 @@
+package at.aau.serg.websocketdemoserver.dto;
+
+public enum MessageType {
+    ROLL_DICE,
+    BUY_PROPERTY,
+    PLAYER_MOVED,
+    CAN_BUY_PROPERTY,
+    MUST_PAY_RENT,
+    PROPERTY_BOUGHT,
+    PAY_TAX,
+    DRAW_EVENT_RISIKO_CARD,
+    DRAW_EVENT_BANK_CARD,
+    GO_TO_JAIL,
+    SKIPPED,
+    LOBBY_UPDATE,
+    START_GAME,
+    INFO,
+    ERROR
+}
