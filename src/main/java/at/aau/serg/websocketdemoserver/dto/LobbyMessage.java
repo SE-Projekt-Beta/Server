19bf0e864ee26c40e1b8f8ea48bda2f7b8dda905
Thomas <thomasel@edu.aau.at>
2025-04-14 12:53:29 +0200

    Create LobbyMessageType enum for lobby message classification

---
+++ src/main/java/at/aau/serg/websocketdemoserver/dto/LobbyMessage.java
@@ -0,0 +1,30 @@
+package at.aau.serg.websocketdemoserver.dto;
+
+public class LobbyMessage {
+    private LobbyMessageType type;
+    private Object payload;
+
+    public LobbyMessage() {}
+
+    public LobbyMessage(LobbyMessageType type, Object payload) {
+        this.type = type;
+        this.payload = payload;
+    }
+
+    public LobbyMessageType getType() {
+        return type;
+    }
+
+    public void setType(LobbyMessageType type) {
+        this.type = type;
+    }
+
+    public Object getPayload() {
+        return payload;
+    }
+
+    public void setPayload(Object payload) {
+        this.payload = payload;
+    }
+}
+
---
+++ src/main/java/at/aau/serg/websocketdemoserver/dto/LobbyMessageType.java
@@ -0,0 +1,8 @@
+package at.aau.serg.websocketdemoserver.dto;
+
+public enum LobbyMessageType {
+    JOIN_LOBBY,
+    START_GAME,
+    ERROR,
+    LOBBY_UPDATE
+}
