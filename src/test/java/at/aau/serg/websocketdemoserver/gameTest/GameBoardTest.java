90f7397f2538850e8b1d954c57aedae669c013d7
Thomas <thomasel@edu.aau.at>
2025-04-25 10:37:28 +0200

    add GameBoardTest for verifying tile list and access by position

---
+++ src/test/java/at/aau/serg/websocketdemoserver/gameTest/GameBoardTest.java
@@ -0,0 +1,28 @@
+package at.aau.serg.websocketdemoserver.gameTest;
+
+import at.aau.serg.websocketdemoserver.model.GameBoard;
+import at.aau.serg.websocketdemoserver.model.Tile;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameBoardTest {
+
+    @Test
+    void testBoardHas40Tiles() {
+        GameBoard board = new GameBoard();
+        for (int i = 0; i < 40; i++) {
+            Tile t = board.getTileAt(i);
+            assertNotNull(t);
+            assertEquals(i, t.getPosition());
+        }
+    }
+
+    @Test
+    void testBoardWrapsCorrectly() {
+        GameBoard board = new GameBoard();
+        Tile t = board.getTileAt(80); // 80 % 40 = 0
+        assertEquals(0, t.getPosition());
+    }
+}
+
