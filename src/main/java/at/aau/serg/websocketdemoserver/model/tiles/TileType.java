64b9f0e0be3a60cf1b29a3035a4274832bb24006
Thomas <thomasel@edu.aau.at>
2025-04-25 17:42:53 +0200

    new enum TileType

---
+++ src/main/java/at/aau/serg/websocketdemoserver/model/tiles/TileType.java
@@ -0,0 +1,13 @@
+package at.aau.serg.websocketdemoserver.model.tiles;
+
+public enum TileType {
+    START,
+    STREET,
+    STATION,
+    EVENT_BANK,
+    EVENT_RISIKO,
+    TAX,
+    FREE,
+    GOTO_JAIL,
+    JAIL
+}
