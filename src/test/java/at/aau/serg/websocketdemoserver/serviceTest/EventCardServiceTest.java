6d55eb90e9dbe2e4df6f770f1203ff2d1047362d
Thomas <thomasel@edu.aau.at>
2025-04-25 10:40:24 +0200

    adapt EventCardServiceTest

---
+++ src/test/java/at/aau/serg/websocketdemoserver/serviceTest/EventCardServiceTest.java
@@ -0,0 +1,29 @@
+package at.aau.serg.websocketdemoserver.serviceTest;
+
+import at.aau.serg.websocketdemoserver.model.tiles.EventCard;
+import at.aau.serg.websocketdemoserver.service.EventCardService;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EventCardServiceTest {
+    @Test
+    void testDrawBankCard() {
+        EventCardService service = new EventCardService();
+        EventCard card = service.drawCard("bank");
+        assertEquals("bank", card.getType());
+    }
+
+    @Test
+    void testDrawRisikoCard() {
+        EventCardService service = new EventCardService();
+        EventCard card = service.drawCard("risiko");
+        assertEquals("risiko", card.getType());
+    }
+
+    @Test
+    void testDrawInvalidCardType() {
+        EventCardService service = new EventCardService();
+        Exception e = assertThrows(IllegalArgumentException.class, () -> service.drawCard("foo"));
+        assertTrue(e.getMessage().contains("Unbekannter Event-Typ"));
+    }
+}
