8f9721cc600c933b29dbbe7986f5d6975e970be0
Thomas <thomasel@edu.aau.at>
2025-05-01 16:35:09 +0200

    new message types after introducing all requests

--- src/main/java/at/aau/serg/websocketdemoserver/dto/MessageType.java
+++ src/main/java/at/aau/serg/websocketdemoserver/dto/MessageType.java
@@ -1,17 +1,36 @@
 package at.aau.serg.websocketdemoserver.dto;
 
 public enum MessageType {
+
+    // Standardaktionen
     ROLL_DICE,
-    BUY_PROPERTY,
-    PLAYER_MOVED,
-    CAN_BUY_PROPERTY,
-    MUST_PAY_RENT,
-    PROPERTY_BOUGHT,
-    PAY_TAX,
-    DRAW_EVENT_RISIKO_CARD,
-    DRAW_EVENT_BANK_CARD,
-    GO_TO_JAIL,
+    MOVE_PLAYER,
+    START_MONEY,
     SKIPPED,
-    ERROR,
-    CURRENT_PLAYER
+
+    // Eigentum und Miete
+    CAN_BUY_PROPERTY,
+    BUY_PROPERTY,
+    MUST_PAY_RENT,
+    PROPERTY_LIST_UPDATE,
+
+    // Sonderfelder & Gefängnis
+    GO_TO_JAIL,
+    PRISON_TASK,
+    PLAYER_OUT_OF_JAIL_CARD,
+    PLAYER_LOST,
+
+    // Ereigniskarten (Bank & Risiko)
+    DRAW_EVENT_BANK_CARD,
+    DRAW_EVENT_RISIKO_CARD,
+
+    // Tasks & Steuern
+    CASH_TASK,
+    PAY_TAX,
+
+    // Spielende
+    END_GAME,
+
+    // Fehler
+    ERROR
 }
