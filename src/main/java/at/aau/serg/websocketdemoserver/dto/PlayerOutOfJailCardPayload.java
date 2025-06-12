79c25854fef4df5bb171f97a69b64381472b8438
Benjamin Bordne <benjibordne@outlook.com>
2025-06-12 10:53:51 +0200

    Refactor PlayerOutOfJailCardPayload to use Lombok for getter and setter methods

--- src/main/java/at/aau/serg/websocketdemoserver/dto/PlayerOutOfJailCardPayload.java
+++ src/main/java/at/aau/serg/websocketdemoserver/dto/PlayerOutOfJailCardPayload.java
@@ -1,29 +1,19 @@
 package at.aau.serg.websocketdemoserver.dto;
 
-public class PlayerOutOfJailCardPayload {
-    private int playerId;
-    private String message;
+import lombok.Getter;
+import lombok.Setter;
 
-    public PlayerOutOfJailCardPayload() {}
+public class PlayerOutOfJailCardPayload {
+    @Getter
+    @Setter
+    private int playerId;
+    @Getter
+    @Setter
+    private String message;
 
     public PlayerOutOfJailCardPayload(int playerId, String message) {
         this.playerId = playerId;
         this.message = message;
     }
 
-    public int getPlayerId() {
-        return playerId;
-    }
-
-    public void setPlayerId(int playerId) {
-        this.playerId = playerId;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
