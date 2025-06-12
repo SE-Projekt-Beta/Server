40478f5fff32bb272fb491f65064e9a5a330fab7
Benjamin Bordne <benjibordne@outlook.com>
2025-06-12 10:52:42 +0200

    remove

--- src/main/java/at/aau/serg/websocketdemoserver/dto/LobbyUpdatePayload.java
+++ src/main/java/at/aau/serg/websocketdemoserver/dto/LobbyUpdatePayload.java
@@ -1,31 +1,22 @@
 package at.aau.serg.websocketdemoserver.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
 public class LobbyUpdatePayload {
+    @Getter
+    @Setter
     private int lobbyId;
+    @Getter
+    @Setter
     private List<PlayerDTO> players;
-
-    public LobbyUpdatePayload() {}
 
     public LobbyUpdatePayload(int lobbyId, List<PlayerDTO> players) {
         this.lobbyId = lobbyId;
         this.players = players;
     }
+    
 
-    public int getLobbyId() {
-        return lobbyId;
-    }
-
-    public void setLobbyId(int lobbyId) {
-        this.lobbyId = lobbyId;
-    }
-
-    public List<PlayerDTO> getPlayers() {
-        return players;
-    }
-
-    public void setPlayers(List<PlayerDTO> players) {
-        this.players = players;
-    }
 }
