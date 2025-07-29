067d27ffbf554008517763702cd8ba59902681d1
Lena Simmerle <lesimmerle@edu.aau.at>
2025-07-29 18:25:36 +0200

    removed old comments

--- src/main/java/at/aau/serg/websocketdemoserver/service/game_request/EmoteRequest.java
+++ src/main/java/at/aau/serg/websocketdemoserver/service/game_request/EmoteRequest.java
@@ -27,9 +27,6 @@ public class EmoteRequest implements GameRequest {
         }
 
         if (!rateLimiter.canSend(sender)) {
-            //Player player = gameState.getCurrentPlayer();
-            //int senderId = player.getId();
-
             Player senderPlayer = gameState.findPlayerByName(sender);
             if (senderPlayer == null) {
                 return MessageFactory.error(lobbyId, "Unbekannter Spieler: " + sender);
